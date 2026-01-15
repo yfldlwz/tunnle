ae916b374348dd7911e74cb5faa161dbd1d74eda
yfldlwz <yfldlwz@gmail.com>
2026-01-15 14:02:00 +0800

    Update index-明码.js
    更新优先域名

--- index-明码.js
+++ index-明码.js
@@ -20,7 +20,7 @@ const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLI
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'galaxy-us.jceg.net';          // 固定隧道域名,留空即启用临时隧道
 const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiMzYwNmNhZGVhMjEyZGNjNjYyMjVlOTA0Njc1M2IxYmIiLCJ0IjoiZjcyYTI2OGUtMmYxNi00ZDRkLWFlNjUtNTlmMGFlOWNmYzI3IiwicyI6Ik9ESmlabUZoTURZdE9HSXhNeTAwTWpsbExUZzVOamt0WVdVNVpXRTJaakZtWVRreCJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
-const CFIP = process.env.CFIP || 'www.gov.se';        // 节点优选域名或优选ip  
+const CFIP = process.env.CFIP || 'cf.877774.xyz';        // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
 const NAME = process.env.NAME || '';                        // 节点名称
 
